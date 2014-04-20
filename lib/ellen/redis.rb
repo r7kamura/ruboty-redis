2a469abea39aa81b0b21f73f07e7c42ea10f11c6
Ryo Nakamura <r7kamura@gmail.com>
2014-04-20 16:40:21 +0900

    bundle gem ellen-redis

---
+++ lib/ellen/redis.rb
@@ -0,0 +1,7 @@
+require "ellen/redis/version"
+
+module Ellen
+  module Redis
+    # Your code goes here...
+  end
+end
---
+++ lib/ellen/redis/version.rb
@@ -0,0 +1,5 @@
+module Ellen
+  module Redis
+    VERSION = "0.0.1"
+  end
+end
